a7bf46fa35e541cefad54e999f75a230ab21f897
zhoudawei <zdw0607@gmail.com>
2023-06-15 18:11:47 +0800

    11

---
+++ aigc-ai-service/src/main/java/com/xcoder/Main.java
@@ -0,0 +1,12 @@
+package com.xcoder;
+
+/**
+ * @Author: zhoudawei
+ * @Time: 2023/6/15 15:47
+ * @Description: description this class usage.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ aigc-common/src/main/java/com/xcoder/Main.java
@@ -0,0 +1,12 @@
+package com.xcoder;
+
+/**
+ * @Author: zhoudawei
+ * @Time: 2023/6/15 14:09
+ * @Description: description this class usage.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
